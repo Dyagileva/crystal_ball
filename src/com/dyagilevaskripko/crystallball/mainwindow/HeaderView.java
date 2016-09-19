586109acce3b7ad956ae4dec0976f0fbe7727822
daryaskripko <daria.skripko@yandex.ru>
2016-09-20 00:42:47 +0300

    add test textfields and labels

--- src/com/dyagilevaskripko/crystallball/mainwindow/HeaderView.java
+++ src/com/dyagilevaskripko/crystallball/mainwindow/HeaderView.java
@@ -1,5 +1,7 @@
 package com.dyagilevaskripko.crystallball.mainwindow;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 /**
@@ -9,6 +11,11 @@ public class HeaderView extends VBox
 {
     public HeaderView()
     {
-        //
+        Label minValueLabel = new Label("MIN_VALUE");
+        TextField minValueTextField = new TextField();
+        Label maxValueLabel = new Label("MAX_VALUE");
+        TextField maxValueTextField = new TextField();
+
+        this.getChildren().addAll(minValueLabel,minValueTextField, maxValueLabel,maxValueTextField);
     }
 }
