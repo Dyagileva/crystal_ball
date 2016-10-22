2fb08debfc3e9aefcefa79203b219b84f76de23c
dyagileva <dyagileva.1995@mail.ru>
2016-10-22 16:58:07 +0300

    change label value

--- src/java/com/dyagilevaskripko/crystallball/circle/CircleView.java
+++ src/java/com/dyagilevaskripko/crystallball/circle/CircleView.java
@@ -16,7 +16,7 @@ public class CircleView extends VBox {
     public CircleView() {
         this.setId("circleStackPane");
 
-        resultLabel = new Label("24");
+        resultLabel = new Label("52");
         resultLabel.setId("resultLabel");
 
         Canvas canvasBigRound = new Canvas(350, 350);
