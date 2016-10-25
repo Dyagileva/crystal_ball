2e19246efe6f9d3c825e6dce72d401cf7b8627b4
origin <dyagileva.1995@mail.ru>
2016-10-25 17:54:32 +0300

    add comment

--- src/java/com/dyagilevaskripko/crystallball/circle/CircleView.java
+++ src/java/com/dyagilevaskripko/crystallball/circle/CircleView.java
@@ -12,7 +12,7 @@ import javafx.scene.paint.Color;
  */
 public class CircleView extends VBox {
     private Label resultLabel;
-
+/**sdfghjkl;**/
     public CircleView() {
         this.setId("circleStackPane");
 
